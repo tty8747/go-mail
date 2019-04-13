ef3e27d29ee33253cade4011ac4bc037ede485a8
Sergey Chulanov <tty8747@gmail.com>
2019-04-13 13:36:42 +0300

    It's working!

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mailgun/mailgun-go"
+)
+
+// Your available domain names can be found here:
+// (https://app.mailgun.com/app/domains)
+var yourDomain string = "example.org" // e.g. mg.yourcompany.com
+
+// You can find the Private API Key in your Account Menu, under "Settings":
+// (https://app.mailgun.com/app/account/security)
+var privateAPIKey string = "your_api_key"
 
 func main() {
-	fmt.Println("vim-go")
+	// Create an instance of the Mailgun Client
+	mg := mailgun.NewMailgun(yourDomain, privateAPIKey)
+
+	sender := "your_name your_email"
+	subject := "Fancy subject!"
+	body := "Hello from Mailgun Go!"
+	recipient := "recipient email"
+
+	// The message object allows you to add attachments and Bcc recipients
+	message := mg.NewMessage(sender, subject, body, recipient)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message	with a 10 second timeout
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
 }
